0112c02bc03e2d59bb34a7e5d3b0b5f63478cf12
bellomusodiq <bmayowa25@gmail.com>
2021-10-15 10:44:47 +0100

    nc

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=225)
+    author = models.CharField(max_length=225)
+    slug = models.CharField(max_length=225)
+
+    def __str__(self):
+        return self.title
